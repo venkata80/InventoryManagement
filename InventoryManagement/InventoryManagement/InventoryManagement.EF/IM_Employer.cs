6c1be0f81ca01e9f2df5645af48ef9f3d61ab06f
subbarao <venkata1982@hotmail.com>
2018-08-22 07:47:06 +0530

    Email added

--- InventoryManagement/InventoryManagement/InventoryManagement.EF/IM_Employer.cs
+++ InventoryManagement/InventoryManagement/InventoryManagement.EF/IM_Employer.cs
@@ -34,5 +34,6 @@ namespace InventoryManagement.EF
         public Nullable<System.DateTime> CreatedDate { get; set; }
         public string ModifiedBy { get; set; }
         public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public string Email { get; set; }
     }
 }
