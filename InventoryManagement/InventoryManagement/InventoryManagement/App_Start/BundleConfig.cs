a6cd39e3ebe98bf3197a8872338b9e54089db32b
subbarao <venkata1982@hotmail.com>
2018-08-26 00:11:29 +0530

    menu items changes

--- InventoryManagement/InventoryManagement/InventoryManagement/App_Start/BundleConfig.cs
+++ InventoryManagement/InventoryManagement/InventoryManagement/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace InventoryManagement
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      //"~/Content/site.css",
+                      "~/Content/style.css"));
         }
     }
 }
--- InventoryManagement/InventoryManagement/InventoryManagement/Controllers/LoginController.cs
+++ InventoryManagement/InventoryManagement/InventoryManagement/Controllers/LoginController.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Security.Principal;
+using System.Web.Security;
 namespace InventoryManagement.Controllers
 {
     public class LoginController : Controller
@@ -34,5 +35,7 @@ namespace InventoryManagement.Controllers
             LoginDTO newlogin = new LoginDTO();
             return View(newlogin);
         }
+
+
     }
 }
