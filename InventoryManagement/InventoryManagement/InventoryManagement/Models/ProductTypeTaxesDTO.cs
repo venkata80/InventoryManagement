2b8862df9f968b5abfccd1e75395665f491f316f
subbarao <venkata1982@hotmail.com>
2018-09-30 08:40:19 +0530

    ProductTypeTaxesDTO

---
+++ InventoryManagement/InventoryManagement/InventoryManagement/Models/ProductTypeTaxesDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Models
+{
+    public class ProductTypeTaxesDTO : CommonBaseDTO<Guid>
+    {
+        public long? ProductTaxeId { get; set; }
+        public long? ProductTypeId { get; set; }
+        public long? SGST { get; set; }
+        public long? CGST { get; set; }
+        public long? IGST { get; set; }
+        public DateTime? AffectiveFrom { get; set; }
+        public DateTime? AffectiveTo { get; set; }
+        public bool Isactive { get; set; }
+    }
+}
